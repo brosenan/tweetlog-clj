2ad3f8bf79e7c5ac157479f21fe4a52794a0cfbc
Boaz Rosenan <brosenan@gmail.com>
2017-04-02 09:03:01 +0300

    updated demo

--- src/tweetlog/core.clj
+++ src/tweetlog/core.clj
@@ -6,19 +6,22 @@
 (perm/pure
  (def app :tweetlog)
 
- (c/defrule timeline-by-tweets [user author tweet]
+ (c/defrule followees-tweets [user author tweet]
    [:tweetlog/follows user author] (c/by [:user= user])
    [:tweetlog/tweeted author tweet] (c/by [:user= author]))
  
- (c/defclause timeline-1 [:tweetlog/timeline user -> author tweet]
-   [timeline-by-tweets user author tweet] (c/by app))
+ (c/defclause tl-1 [:tweetlog/timeline user -> author tweet]
+   [followees-tweets user author tweet] (c/by app))
 
  (c/defrule mention [mentioned author tweet]
    [:tweetlog/tweeted author tweet] (c/by [:user= author])
-   (for [token (str/split tweet #"[ .,:!?]+")])
+   (for [token (str/split tweet #"[ .,;?!]+")])
    (when (str/starts-with? token "@"))
    (let [mentioned (subs token 1)]))
 
- (c/defclause timeline-2 [:tweetlog/timeline user -> author tweet]
+ (c/defclause tl-2 [:tweetlog/timeline user -> author tweet]
    [mention user author tweet] (c/by app)
    (let [tweet (str "You were mentioned: " tweet)])))
+
+
+
--- test/tweetlog/core_test.clj
+++ test/tweetlog/core_test.clj
@@ -9,20 +9,24 @@
 (defn query [facts q arity readers]
   (apply c/run-query rules q arity app readers facts))
 
+
 (def story
-  [(c/f [:tweetlog/tweeted "maui" "I'm a demigod!"] :writers #{[:user= "maui"]})
+  [(c/f [:tweetlog/tweeted "maui" "I'm a #demigod!"] :writers #{[:user= "maui"]})
    (c/f [:tweetlog/follows "moana" "maui"] :writers #{[:user= "moana"]})
    (c/f [:tweetlog/tweeted "tamatoa" "I'm so shiny!"] :writers #{[:user= "tamatoa"]})
    (c/f [:tweetlog/follows "moana" "tamatoa"] :writers #{[:user= "tamatoa"]})
-   (c/f [:tweetlog/tweeted "maui" "@tamatoa's so shiny!"] :writers #{[:user= "tamatoa"]})
-   (c/f [:tweetlog.core/timeline-by-tweets "moana" "tamatoa" "I'm so shiny!"] :writers #{[:user= "tamatoa"]})
-   (c/f [:tweetlog/tweeted "moana" "Hey, @maui, we need to save the world!"] :writers #{[:user= "moana"]})
-   (c/f [:tweetlog/tweeted "maui" "Need to get my hook from @tamatoa"]
+   (c/f [:tweetlog/tweeted "maui" "@tamatoa is so shiny!"] :writers #{[:user= "tamatoa"]})
+   (c/f [:tweetlog.core/followees-tweets "moana" "tamatoa" "I'm so shiny!"] :writers #{[:user= "tamatoa"]})
+   (c/f [:tweetlog/tweeted "moana" "Hey @maui, time to save the world!"] :writers #{[:user= "moana"]})
+   (c/f [:tweetlog/tweeted "maui" "I first need to grab my hook from @tamatoa"]
         :writers #{[:user= "maui"]}
         :readers #{[:user= "moana"]})])
 
 (fact
- (query story [:tweetlog/timeline "moana"] 2 #{[:user= "moana"]}) => #{["maui" "I'm a demigod!"]
-                                                       ["maui" "Need to get my hook from @tamatoa"]}
- (query story [:tweetlog/timeline "maui"] 2 #{[:user= "maui"]}) => #{["moana" "You were mentioned: Hey, @maui, we need to save the world!"]}
- (query story [:tweetlog/timeline "tamatoa"] 2 #{[:user= "tamatoa"]}) => #{})
+ (query story [:tweetlog/timeline "moana"] 2 #{[:user= "moana"]})
+ => #{["maui" "I'm a #demigod!"]
+      ["maui" "I first need to grab my hook from @tamatoa"]}
+ (query story [:tweetlog/timeline "maui"] 2 #{[:user= "maui"]})
+ => #{["moana" "You were mentioned: Hey @maui, time to save the world!"]}
+ (query story [:tweetlog/timeline "tamatoa"] 2 #{[:user= "tamatoa"]})
+ => #{})
